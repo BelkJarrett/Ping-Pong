b7acd20a425ce3156373cb172bde89c39659ed09
BelkJarrett <belkjarrett@gmail.com>
2022-09-28 15:11:41 -0400

    Ping Pong Game

---
+++ Assets/script/GenericBehavoir.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace muninum
+{
+   [System.Serializable]
+   public class Genericforces
+    {
+        public Vector3 force;
+        public Vector3 impulse;
+        public Vector3 acceleration;
+        public Vector3 velocitychange;
+
+        public Genericforces()
+        {
+            force = Vector3.zero;
+            impulse = Vector3.zero;
+            acceleration = Vector3.zero;
+            velocitychange = Vector3.zero;
+        }
+
+        public void forceZero()
+        {
+            force = Vector3.zero;
+        }
+
+        public void forceSet(Vector3 t)
+        {
+            force = t;
+        } 
+
+         public void impulseZero()
+        {
+            impulse = Vector3.zero;
+        }
+
+        public void impulseSet(Vector3 t)
+        {
+
+        }
+    }
+}
---
+++ Assets/script/Goal.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Goal : MonoBehaviour
+{
+    public Text myScoreText;
+    private int Score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   void OnCollisionEnter(Collision other)
+    {
+      
+        other.transform.position = Vector3.zero;
+
+        Score += 1;
+        //Score = Score + 1;
+        ///Score++;
+
+        myScoreText.text = Score.ToString();
+
+        if(Score == 10)
+        {
+            SceneManager.LoadScene("PongScene");
+        }
+    }
+}
